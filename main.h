4888a7df91cd5ccf774545c12c89fbbd82183696
tarek <tahdeeb@yahoo.com>
2023-04-24 22:39:54 -0700

    functions, functions1, functions2, get_size and get_precision

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 3
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
+#endif
