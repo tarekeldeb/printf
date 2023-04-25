6fb4f69c13b1ca2b09ad3948a73197ef2acdc559
tarek <tahdeeb@yahoo.com>
2023-04-24 22:41:13 -0700

    main.h

--- main.h
+++ main.h
@@ -17,4 +17,27 @@
 #define S_LONG 2
 #define S_SHORT 1
 
+
+
+
+int _printf (const char *format, ...);
+void printBuffer(char buffer[], int *bufferIndex);
+int printChar(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printString(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printPercent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printInt(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printBinary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printUnsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printOctal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printHexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printHexaUpper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printHexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int printPointer(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printNonPrintable(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printReverse(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printRot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int getSize(const char *format, int *i);
+int getPrecision(const char *format, int *i, va_list list);
+
+
 #endif
