110155780c7d8e8121fe78cea33efc517048a095
MohamedEmad34 <me632453@gmail.com>
2023-04-25 02:49:01 -0700

    mod

--- main.h
+++ main.h
@@ -16,9 +16,19 @@
 
 #define S_LONG 2
 #define S_SHORT 1
-
-
-
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+}fmt_t;
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 int _printf (const char *format, ...);
 void printBuffer(char buffer[], int *bufferIndex);
@@ -38,6 +48,17 @@ int printReverse(va_list types, char buffer[], int flags, int width, int precisi
 int printRot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int getSize(const char *format, int *i);
 int getPrecision(const char *format, int *i, va_list list);
-
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char buffer[], int flags, int width, int prec, int length, char padd, char extra_c);
+int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int is_printable(char c);
+int append_hexa_code(char ascii_code, char buffer[], int i);
+int is_digit(char c);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+int getWidth(const char *format, int *i, va_list list);
+int getFlags(const char *format, int *i);
 
 #endif
