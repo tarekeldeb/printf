66b883fe627a2ab1e446ad3bb4bb4f0b255ced24
MohamedEmad34 <me632453@gmail.com>
2023-04-25 02:10:39 -0700

    get_width.c handle_print.c utils.c write_handlers.c get_flags.c

---
+++ get_flags.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* get_flags - Calculates active flags
+* @format: Formatted string in which to print the arguments
+* @i: take a parameter.
+* Return: Flags:
+*/
+int getFlags(const char *format, int *i)
+{
+	int j, curri;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curri = *i + 1; format[curri] != '\0'; curri++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+		{
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+		}
+		if (FLAGS_CH[j] == 0)
+			break;
+
+	}
+	*i = curri - 1;
+	return (flags);
+}
